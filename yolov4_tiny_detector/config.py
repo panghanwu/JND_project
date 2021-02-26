39c6af11a3c2a26a69d247deea46b39986d01ef4
Keyboy Wu <66063216+panghanwu@users.noreply.github.com>
2021-02-26 16:54:34 +0800

    Add files via upload

---
+++ yolov4_tiny_detector/config.py
@@ -0,0 +1,45 @@
+class Config:
+    
+    def __init__(self):
+        self.model_name = 'YOLOv4 tiny'
+        self.input_size = (416, 416)  
+        
+        # classes
+        self.class_names = [
+            '0',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9'
+        ]
+
+        # anchors
+        self.anchors = [
+            (10,14),  (23,27),  (37,58),  (81,82),  (135,169),  (344,319)
+        ]    
+
+            
+
+class DetectorConfig(Config):
+
+    def __init__(self):
+        # get general configs
+        super(DetectorConfig, self).__init__()
+        # threshold
+        self.confidence = 0.5
+        self.iou = 0.3
+        self.text_font = 'simhei.ttf'
+        
+        
+        
+class TrainingConfig(Config):
+    
+    def __init__(self):
+        # get general configs
+        super(TrainingConfig, self).__init__()
+        pretrain_weights = None
